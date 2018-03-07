92815eb031bb7161618d6cf56f8a9cec3c3e8cd0
joechesney <33613119+joechesney@users.noreply.github.com>
2018-03-06 19:32:37 -0600

    redid the exercise to use sync mug

--- file-io.js
+++ file-io.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
-
-fs.readFile('./file.json', 'utf8', (error, data)=>{
-  // console.log(data);
+try{
+  let data = fs.readFileSync('./file.json', 'utf8');
   process.stdout.write(data+"\n");
-});
+}catch(error){
+  process.stdout.write(error+"\n");
+
+}
